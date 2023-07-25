3e2060a32835ba018c4a7be3f3dc6767ee1652dd
wwwxsv19 <wwwxsv19@gmail.com>
2023-07-25 12:08:19 +0900

    python codeup

---
+++ python0725.py
@@ -0,0 +1,23 @@
+import turtle as t
+
+ub = t.Turtle()
+ub.shape("turtle")
+ub.color("blue")
+
+ub.fillcolor('skyblue')
+ub.begin_fill()
+ub.circle(100)
+ub.end_fill()
+
+ub.rt(90)
+ub. fd(50)
+ub.rt(90)
+
+ub.fillcolor('skyblue')
+ub.begin_fill()
+ub.circle(100)
+ub.end_fill()
+
+t.bgcolor('pink')
+ub.clear()
+ub.reset()
